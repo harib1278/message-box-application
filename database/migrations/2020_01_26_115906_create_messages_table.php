39af04f3172e67f1c1472fb71e6a56c6bb867870
harib128 <tomshaddock@googlemail.com>
2020-01-26 13:07:20 +0100

    Addition of the auth ui elements

---
+++ database/migrations/2020_01_26_115906_create_messages_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('user_id_from');
+            $table->integer('user_id_to');
+            $table->string('subject');
+            $table->string('body');
+            $table->boolean('read');
+            $table->boolean('deleted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
